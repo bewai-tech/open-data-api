47189c7ed90e582088a05e3931aa8d7fdf5ffb32
Maxime Lafarie <maxime.lafarie@gmail.com>
2023-11-28 10:58:27 +0100

    feat: add siren filter on sirene route returning latest entry

--- src/controllers/siren.controller.ts
+++ src/controllers/siren.controller.ts
@@ -8,16 +8,18 @@ class SirenController {
      * @returns the corresponding item(s)
      */
     static checkSirene = async (req: Request, res: Response) => {
-        const { siret } = req.query as Record<any, string>;
+        const { siret, siren } = req.query as Record<any, string>;
         const limit = +req.query?.limit <= 100 ? +req.query?.limit : 100;
         const offset = +req.query?.offset || 0;
 
         let result;
         try {
-            if (!siret) {
-                result = await db.any(`SELECT * FROM sirene LIMIT ${limit} OFFSET ${offset}`);
-            } else {
+            if (siret) {
                 result = await db.one('SELECT * FROM sirene WHERE siret = $1', [siret]);
+            } else if (siren) {
+                result = await db.one('SELECT * FROM sirene WHERE siren = $1 AND nic = (SELECT MAX(DISTINCT nic) FROM sirene WHERE siren = $1)', [siren]);
+            } else {
+                result = await db.any(`SELECT * FROM sirene LIMIT ${limit} OFFSET ${offset}`);
             }
         } catch (error) {
             res.sendStatus(404);
